fbd73c846c60ae7e7cfba2c13ac1dc442e8c5c9d
pan.changzhi <pan.changzhi@zhaopin.com.cn>
2019-03-25 16:01:21 +0800

    返回promise

---
+++ src/http/promiseIndex.js
@@ -0,0 +1,36 @@
+import Axios from './axios.config'
+
+/**
+ * 请求方法
+ *
+ * @param {String}    url      接口地址
+ * @param {String}    methods  请求方法
+ * @param {Object}    headers  headers 设置
+ * @param {Object}    data     参数
+ * @param {Boolean}   silent   静默模式
+ *
+ */
+
+export default function request({ url = '', method = 'get', data = {}, params = {}, headers = {}, silent = false } = {}) {
+  // console.log(`${url}============>`, methods, data, silent)
+  const id = `${url}TIME${new Date().getTime()}` // 生成id
+  if (!silent) {
+    // 非静默模式，压入请求队列
+    Axios.setRequestList({ id })
+  }
+
+  return Axios.axios({
+    url,
+    method,
+    headers,
+    data,
+    params,
+  })
+    .then(() => {
+      Axios.setRequestList({ el: id, remove: true }) // 请求完成后，出队
+    })
+    .catch(error => {
+      Axios.setRequestList({ el: id, remove: true }) // 请求完成后，出队
+      if (!silent) Axios.Toast(error.msg || error.message || error)
+    })
+}
